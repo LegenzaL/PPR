9afbd503638281cd85c0406b7e2c7a7e043c30d6
Ligia Legenza <66174660+LegenzaL@users.noreply.github.com>
2021-04-18 17:55:48 +0200

    Add files via upload

--- serwer.cs
+++ serwer.cs
@@ -11,7 +11,8 @@ namespace socket
         {
 			byte[] bytes = new Byte[1024];
 			string data = null;
-			IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 11000);
+            
+			IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 20000);
 			Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp );
 			try {
 				listener.Bind(localEndPoint);
@@ -21,11 +22,13 @@ namespace socket
 						Console.WriteLine("Waiting for a connection...");
 						Socket handler = listener.Accept();
 						Console.WriteLine($"Connection from {handler.RemoteEndPoint.ToString()}");
-						handler.Receive(bytes);
-						data = Encoding.Unicode.GetString(bytes);
-						Console.WriteLine($"Received: {data}, sending it back.");
-						byte[] msg = Encoding.Unicode.GetBytes(data);
-						handler.Send(msg);
+
+                        handler.Receive(bytes);
+                        data = BitConverter.ToString(bytes);
+
+
+						Console.WriteLine($"Received: {data}");
+						handler.Send(bytes);
 						handler.Shutdown(SocketShutdown.Both);
 						handler.Close();
 					} catch (Exception e) {
