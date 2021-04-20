f1281cda1776b31c6a75ebffa148cb5e768a189d
Ligia Legenza <66174660+LegenzaL@users.noreply.github.com>
2021-04-20 15:44:59 +0200

    Create client3.2.py

---
+++ Lab1/Zadanie3/client3.2.py
@@ -0,0 +1,13 @@
+import socket
+import sys
+   
+ip = "127.0.0.1"
+port = 21000
+message = raw_input("Message: ")
+   
+print("UDP target IP: %s" % ip)
+print("UDP target port: %s" % port)
+print("message: %s" % message)
+  
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(message, (ip, port))    
