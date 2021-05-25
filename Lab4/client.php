f82da3fa888964f5c81161e9f311179ff866378a
Ligia Legenza <66174660+LegenzaL@users.noreply.github.com>
2021-05-25 17:16:17 +0200

    Update client.php

--- Lab4/client.php
+++ Lab4/client.php
@@ -6,22 +6,15 @@
 	$host 	= '127.0.0.1';
 	$wsdl 	= 'http://' . $host . ':' . $port . '?wsdl';
 	#-------------------------------------------------------------------
-	$soap = new SoapClient( $wsdl, array( 'proxy_host'   => "127.0.0.1",
-                                          'proxy_port'   => 7777) );
-	#$res  = $soap->getTime( 'Some data...' );
-	$liczba1 = readline("Liczba1: ");
-    readline_add_history($liczba1);
-    #echo "$liczba1";
-	#$soap->getTime( "$liczba1" );
+	$soap = new SoapClient( $wsdl, array( 'proxy_host'   => "127.0.0.1", 'proxy_port'   => 7777) );
 	
-	$liczba2 = readline("Liczba2: ");
-    readline_add_history($liczba2);
-    #echo "$liczba2";
-	$soap->getTime( "$liczba1", "$liczba2" );
+	$liczby[0]=$argv[1];
+	$liczby[1]=$argv[2];
 	
+	#print($liczby[0]);
+	#print($liczby[1]);
 	
-	
-	#print_r( $soap->getTime( "$line" ) );
+	print_r( $soap->getTime( $liczby ) );
 	
 	#===================================================================
 ?>
