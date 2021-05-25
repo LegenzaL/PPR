e1b8ce3e18761015a903d02881e1d6a7b24820a6
Ligia Legenza <66174660+LegenzaL@users.noreply.github.com>
2021-05-25 17:35:15 +0200

    Create client.php

---
+++ Lab4/Zadanie2/client.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php
+
+<?php
+	#===================================================================
+	$port 	= 7777;
+	$host 	= '127.0.0.1';
+	$wsdl 	= 'http://' . $host . ':' . $port . '?wsdl';
+	#-------------------------------------------------------------------
+	$soap = new SoapClient( $wsdl, array( 'proxy_host'   => "127.0.0.1", 'proxy_port'   => 7777) );
+	
+	
+	
+	$message=readline(' Podaj liczbe do konwersji: ');
+	
+	print($message);
+	
+	
+	
+	print_r( $soap->getTime( $liczby ) );
+	
+	#===================================================================
+?>
