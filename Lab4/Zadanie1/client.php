8d9b35816481a82df358a5f0a6dcd2b0e20b9e83
Ligia Legenza <66174660+LegenzaL@users.noreply.github.com>
2021-05-25 19:49:48 +0200

    Update client.php

--- Lab4/Zadanie1/client.php
+++ Lab4/Zadanie1/client.php
@@ -4,19 +4,19 @@
 	#===================================================================
 	$port 	= 7777;
 	$host 	= '127.0.0.1';
-	$wsdl 	= 'http://' . $host . ':' . $port . '?wsdl';
+	$wsdl 	= 'http://' . $host . ':' . $port . '/?wsdl';
 	#-------------------------------------------------------------------
 	$soap = new SoapClient( $wsdl, array( 'proxy_host'   => "127.0.0.1", 'proxy_port'   => 7777) );
 	
+	$liczby[0]=$argv[1];
+	$liczby[1]=$argv[2];
 	
-	
-	$message=readline(' Podaj liczbe do konwersji: ');
-	
-	print($message);
-	
-	
+	#print($liczby[0]);
+	#print($liczby[1]);
 	
 	print_r( $soap->getTime( $liczby ) );
 	
+	
+	print("\n\n");
 	#===================================================================
 ?>
